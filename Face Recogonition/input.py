b01a74d47e2ea4f8eab6d76241e4290eb0520909
Dark-knight-02 <pranav.s.cd.mat20@itbhu.ac.in>
2021-10-31 19:36:04 +0530

    adding files

---
+++ Face Recogonition/input.py
@@ -0,0 +1,16 @@
+import cv2
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read()
+
+    if not ret:
+        continue
+
+    cv2.imshow("Video Input", frame)
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
